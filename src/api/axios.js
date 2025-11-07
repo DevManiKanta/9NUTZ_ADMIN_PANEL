dd1bc7fa821d78a2504fb494d8679f5d4bfad2d2
DevManikanta <dmk.mani321@gmail.com>
2025-11-07 19:42:18 +0530

    new changes

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,9 +2,9 @@
 import axios from "axios";
 
 // export const BASE_URL_1 = "http://192.168.29.8:8000/api";
-// const BASE_URL_2 = "https://9nutsapi.nearbydoctors.in/public/api";
+const BASE_URL_2 = "https://9nutsapi.nearbydoctors.in/public/api";
 // const BASE_URL_2 = "https://confidays.nearbydoctors.in/public/api";
-const BASE_URL_2 = "https://api-mandala.nearbydoctors.in/public/api";
+// const BASE_URL_2 = "https://api-mandala.nearbydoctors.in/public/api";
 
 const api = axios.create({
   baseURL: BASE_URL_2,
